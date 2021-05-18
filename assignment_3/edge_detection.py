1267747a2e49c226ba071fae20e902e47d8298db
JohanHorsmans <54767010+JohanHorsmans@users.noreply.github.com>
2021-05-18 12:46:11 +0200

    Updated assignment 3

--- assignment_3/edge_detection.py
+++ assignment_3/edge_detection.py
@@ -21,7 +21,7 @@ custom_oem_psm_config = r'--oem 2 --psm 4'
 
 #Define function argument defaults and how to specify them from the terminal:
 ap = argparse.ArgumentParser(description = "[DESCRIPTION]: A function designed to read text from image-files. The following parameters can be specified but you can also run the code with default parameters:")
-ap.add_argument("-i", "--image_path", default = os.path.join("data","WHTT.jpg"), type = str, help = "string, path to input file. [DEFAULT]: os.path.join(\"data\", \"WHHT.jpg\")")
+ap.add_argument("-i", "--image_path", default = os.path.join("data","WHTT.jpg"), type = str, help = "string, path to input file. Be weary of difference in operating systems in terms of spcifying path with \" / \" or \" \ \" [DEFAULT]: data/WHHT.jpg")
 ap.add_argument("-l", "--lower", default = 110, type = int, help = "integer, lower limit for thresholding (i.e. if pixel < 0 make it white). [DEFAULT]: 110")
 ap.add_argument("-u", "--upper", default = 255, type = int, help = "integer, lower limit for thresholding (i.e. if pixel > 0 make it white). [DEFAULT]: 255")
 
@@ -49,7 +49,7 @@ def main(image_path, lower, upper):
                                    (0,0,200), #Contour colout
                                    2) #contour pixel width
     
-    print("[INFO]: Writing \"image_letters.jpg\"")
+    print("[INFO]: Writing \"image_letters.jpg\" to the \"data\"-folder")
     #Writing file:
     filepath, _ = os.path.split(image_path) #Specifying that the image should be saved in the same folder as specified in the image_path
     outfile = os.path.join(filepath, "image_letters.jpg") #Specifying that the image should be titled "image_letter.jpg"
@@ -104,13 +104,14 @@ def main(image_path, lower, upper):
     wordsegment.load()
     
     #Print the text in the terminal
-    print(f"[INFO]: The text on the image says: {ocr_correct(text)}") 
+    print(f"[INFO]: The text on the image says: \"{ocr_correct(text)}\"") 
     
     #Write the text to a .txt-file (with the same name as the image).
     filename = Path(image_path).stem
     doc = open(f"{filename}_text.txt", "w")
     doc.write(f"{ocr_correct(text)}")
     doc.close()
+    print(f"[INFO]: \"{filename}_text.txt\" successfully written to the \"assignment_3\"-folder") 
     
 if __name__ =="__main__":
     main(
