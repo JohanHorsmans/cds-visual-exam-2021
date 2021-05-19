a78da714a4a5c8a45c77e8bfd39f3fb00d42b3d8
JohanHorsmans <54767010+JohanHorsmans@users.noreply.github.com>
2021-05-20 01:35:40 +0200

    Update create_visual_venv.sh

--- create_visual_venv.sh
+++ create_visual_venv.sh
@@ -8,8 +8,13 @@ pip install --upgrade pip
 
 pip install ipython
 pip install jupyter
+
+# Loading these here, since they are giving trouble in the requirements.txt
 pip install matplotlib
 pip install opencv-python
+pip install pydot
+pip install pydotplus
+pip install graphviz
 
 python -m ipykernel install --user --name=$VENVNAME
 
